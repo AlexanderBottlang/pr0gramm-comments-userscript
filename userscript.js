4248842f8259bcac2855d96da99701829030783c
Alexander Bottlang <alexander.bottlang@gmail.com>
2015-11-21 11:51:35 -0600

    Moved between score and timestamp
    Moved the heart between the score and timestamp, similar to the
    post-favorite.

--- userscript.js
+++ userscript.js
@@ -4,7 +4,7 @@
 // @version      0.1
 // @description  Lets you set and un-set each comment as a favorite (using Mopsalarm's Service)
 // @author       77
-// @match        http://pr0gramm.com/
+/*// @match        http://pr0gramm.com/*/
 // @grant        none
 // ==/UserScript==
 /* jshint -W097 */
@@ -12,12 +12,12 @@
 
 $(function() {
 
-var klick = "spoiler(this); function spoiler(element) {   var spoiler = element.parentNode.parentNode.nextElementSibling;   if (spoiler.style.display === 'none') {     spoiler.style.display = 'block';     element.innerHTML = '<span class=pict vote-fav style=font-size:14px; position:static;>*</span>';   }else{     spoiler.style.display = 'none';     element.innerHTML = '<span class=pict vote-fav faved style=font-size:14px; position:static;>*</span>';   } }";
-var code = '<span class="pict">r</span> antworten</a>  <a class="action" onclick="' + klick + '"><span class="pict vote-fav" style="font-size:14px; position:static;">*</span></a>';
 
+var klick = "spoiler(this); function spoiler(element) {   var spoiler = element.parentNode.parentNode.nextElementSibling;   if (spoiler.style.display === 'none') {     spoiler.style.display = 'block';     element.innerHTML = '<span class=pict vote-fav style=font-size:14px; position:static;>*</span>';   }else{     spoiler.style.display = 'none';     element.innerHTML = '<span class=pict vote-fav faved style=font-size:14px; position:static;>*</span>';   } }";
+var code = '<a class="action" onclick="' + klick + '"><span class="pict vote-fav" style="font-size:14px; position:static;">*</span></a>';
 
 p.View.Stream.Comments.prototype.template =
 p.View.Stream.Comments.prototype.template
-.replace('<span class="pict">r</span> antworten</a> <?js } ?>', code + '<?js } ?>');
+.replace('</span> <?js } ?> <a href', '</span> <?js } ?> ' + code + ' <a href');
 
 });
